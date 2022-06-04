4dc859ed5826bf0ac4bff57d3e4048647c5dacf5
Moin <kapil.moin@capgemini.com>
2022-06-04 14:08:51 +0530

    commit

--- src/main/java/com/capgemini/demo4/App.java
+++ src/main/java/com/capgemini/demo4/App.java
@@ -8,8 +8,9 @@ public class App
 {
     public static void main( String[] args )
         //kapil moin
+    // this recived
     {
         System.out.println( "Hello World!" );
-        //thanx kapil
+       
     }
 }
